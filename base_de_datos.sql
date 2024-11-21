3fb855c0ce0d1e3620541b3e19d38312c968cf44
karen <karen.lopez2@catolica.edu.sv>
2024-11-21 15:08:30 -0600

    base

--- base_de_datos.sql
+++ base_de_datos.sql
@@ -55,9 +55,30 @@ CREATE TABLE citas (
     fecha_cita DATETIME,
     FOREIGN KEY (dui) REFERENCES persona(dui)
 );
+-- Listado de licencias
+INSERT INTO licencias(categoria, edad_requerida) values 
+("Licencia Juvenil Vehículos Particulares",15),
+("Licencia Juvenil Motociclistas",15),
+("Licencia Motociclistas",18),
+("Licencia Particular",18),
+("Licencia Pesada",18),
+("Licencia Pesada-T",21);
 
-INSERT INTO licencias(categoria, edad_requerida) values ("Licencia Juvenil Vehículos Particulares",15),("Licencia Juvenil Motociclistas",18),
-("Licencia Motociclistas",18),("Licencia Particular",18),("Licencia Pesada",18),("Licencia Pesada-T",21);
-
-ALTER TABLE persona ADD COLUMN tipo_usuario VARCHAR(5);
-ALTER TABLE persona ADD COLUMN contra VARCHAR(20)
+ALTER TABLE persona ADD COLUMN tipo_usuario VARCHAR(10);
+ALTER TABLE persona ADD COLUMN contra VARCHAR(20);
+-- Listado de infracciones
+INSERT INTO infracciones (nombre, clasificacion, tarifa) VALUES
+('Estacionarse en lugar prohibido', 'Leve', 11.43),
+('No portar licencia de conducir', 'Grave', 34.29),
+('Conducir con las luces apagadas en la noche', 'Grave', 34.29),
+('Conducir bajo efectos de alcohol', 'Muy Grave', 57.14),
+('Pasar un semáforo en rojo', 'Muy Grave', 57.14),
+('No respetar señales de tránsito', 'Grave', 34.29),
+('Usar el teléfono celular mientras conduce', 'Grave', 34.29),
+('No usar el cinturón de seguridad', 'Leve', 11.43),
+('Sobrepasar el límite de velocidad', 'Muy Grave', 57.14),
+('Conducir un vehículo sin placas', 'Grave', 34.29),
+('Transportar exceso de pasajeros', 'Grave', 34.29),
+('Circular en contravía', 'Muy Grave', 57.14),
+('Estacionarse en zona para discapacitados', 'Muy Grave', 57.14),
+('No detenerse ante un paso peatonal', 'Grave', 34.29);
